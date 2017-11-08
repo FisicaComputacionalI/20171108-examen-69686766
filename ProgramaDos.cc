c872177f8cff9281803e5136e1ff4ba610288d30
Crescenciano Cuautle <31351800+69686766@users.noreply.github.com>
2017-11-08 13:52:56 -0600

    Add files via upload

---
+++ ProgramaDos.cc
@@ -0,0 +1,21 @@
+//Crescenciano Cuautle Coyotl
+//Segundo Examen Parcial 
+//Programa Dos
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x;
+  x=1;
+
+ while(x<=5){
+ if(x%3==0){     
+ x=x+2;
+}
+    else{
+      cout<<"x"<<endl;
+      x=x+2;}
+  }
+  return 0;
+}
---
+++ ProgramaTres.cc
@@ -0,0 +1,19 @@
+//Crescenciano Cuautle Coyotl
+//Segundo Examen Parcial
+//Programa Tres
+
+#include<iostream>
+using namespace std;
+int main(){
+
+ float D;
+ float T;
+ float V;
+ cout<<"Cual fue la distancia que recorrio en metros: "<<endl;
+ cin>>D;
+ cout<<"Escriba cuanto tiempo le tomo recorrer la distancia en segundos"<<endl;
+ cin>>T;
+ V=D/T;
+ cout<<"La velocidad es "<<V<<" metros sobre segundos"<<endl;
+ return 0;
+}
